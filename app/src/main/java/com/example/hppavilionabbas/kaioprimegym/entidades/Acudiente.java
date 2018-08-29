d6ab27a0673d4f5d3537febe94dff0c222ead2a9
Nando98 <hdoroguezrico98@gmail.com>
2018-08-28 22:44:40 -0500

    Creacion de DAO usuario y acudiente, creacion de clase para la conexión a la BD

---
+++ app/src/main/java/com/example/hppavilionabbas/kaioprimegym/entidades/Acudiente.java
@@ -0,0 +1,4 @@
+package com.example.hppavilionabbas.kaioprimegym.entidades;
+
+public class Acudiente {
+}
---
+++ app/src/main/java/com/example/hppavilionabbas/kaioprimegym/entidades/Usuario.java
@@ -0,0 +1,129 @@
+package com.example.hppavilionabbas.kaioprimegym.entidades;
+
+public class Usuario
+{
+
+    private int id;
+    private String nombre;
+    private String apellidos;
+    private String cedula;
+    private String edad;
+    private String sexo;
+    private String telefono;
+    private String contrasena;
+    private String correo;
+    private String fechaNacimiento;
+    private String foto;
+
+    public Usuario(int id, String nombre, String apellidos,
+                   String cedula, String edad, String sexo, String telefono, String contrasena,
+                   String correo, String fechaNacimiento, String foto) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.cedula = cedula;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.telefono = telefono;
+        this.contrasena = contrasena;
+        this.correo = correo;
+        this.fechaNacimiento = fechaNacimiento;
+        this.foto = foto;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Acudiente getAcudiente() {
+        return acudiente;
+    }
+
+    public void setAcudiente(Acudiente acudiente) {
+        this.acudiente = acudiente;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+}
